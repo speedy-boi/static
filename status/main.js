5b50bad1833f5cddde4d04883e68f1f33e284842
George <g@speedyboi.com>
2021-01-05 21:48:18 +0000

    Added dynamic status page

---
+++ status/main.js
@@ -0,0 +1,27 @@
+let setValues = (inputDict) => {
+    document.querySelector("#emoji").innerHTML = inputDict.emoji;
+    document.querySelector("#title").innerHTML = inputDict.title;
+    document.querySelector("#description").innerHTML = inputDict.description;
+}
+
+let statusDict = {
+    "burnt": {
+        emoji: "🔥",
+        title: "Burnt",
+        description: "Data successfully burnt"
+    },
+    "unknown": {
+        emoji: "🤷‍♂️",
+        title: "Unknown",
+        description: "An unknown message was provided"
+    }
+}
+
+const urlParams = new URLSearchParams(window.location.search);
+const status = urlParams.get('msg');
+
+if (status in statusDict) {
+    setValues(statusDict[status])
+} else {
+    setValues(statusDict["unknown"])
+}
